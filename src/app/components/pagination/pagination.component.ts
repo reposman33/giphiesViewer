e786fbdfdaaaad1509462e39c32c5b2c8aded75f
Marc Bakker <webdev@marcbakker.com>
2020-07-07 17:19:37 +0200

    Add pagination: add next(> >>) and previous (< <<) page buttons & more...

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -14,15 +14,17 @@ import { APIService } from '../../services/api.service';
   styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent {
-  @Input() count: number; // number of items retrieved...
+  @Input() count: string; // number of items retrieved...
   @Input() offset: number; // starting from this index
-  @Input() total_count: number; // the total number of items available
+  @Input() total_count: string; // the total number of items available
   @Output() getDataEvent = new EventEmitter<number>();
   nrOfPages: number = 0;
+  maxNrOfPages: number = 25;
   firstPage: number = 1;
-  lastPage: number = this.firstPage + this.count - 1;
+  lastPage: number;
   // this allows a class to be applied to the selected page-item in the template
   selectedPage = this.firstPage;
+  nrOfPageButtons: number = 5;
 
   constructor(private http: APIService) {}
 
@@ -30,16 +32,21 @@ export class PaginationComponent {
    *
    * @param changes {SimpleChange} - the values of input properties count, offset and total_count
    * @returns void
-   * @description - when data is async retrieved by parentcomponent, values for count and total_count change. So update nrOfPages here
+   * @description - when data is async retrieved by parentcomponent, values for count and total_count change. So assign those values here.
    */
   ngOnChanges(changes: SimpleChange) {
-    this.count = parseInt(changes['count'].currentValue);
-    const totalCount = parseInt(changes['total_count'].currentValue);
-    this.nrOfPages =
-      changes['count'].previousValue != this.count &&
-      changes['total_count'].previousValue != totalCount
-        ? Math.ceil(totalCount / this.count)
-        : this.nrOfPages;
+    //  CurrentValue of count only differs from assigned values after the first request. On subsequent requests only input parameter'offset' changes and thus is the only property available in the changes object
+    const count = changes['count']
+      ? parseInt(changes['count'].currentValue)
+      : parseInt(this.count);
+
+    // CurrentValue of total_count only differs from assigned value after the first request. After that only input parameter 'offset' changes and  is the only proprety available in the changes object.
+    const totalCount = changes['total_count']
+      ? parseInt(changes['total_count'].currentValue)
+      : parseInt(this.total_count);
+    this.nrOfPages = Math.min(totalCount, this.maxNrOfPages);
+
+    this.lastPage = this.firstPage + count - 1;
   }
 
   /**
@@ -60,19 +67,27 @@ export class PaginationComponent {
    * @description - calculate the new pagebuttons and call parent component to retrieve new data
    */
   getPage(page: number) {
-    // clicking on the first or last pagebutton shifts the available pagebuttons 1 position.
-    const pageShift =
-      page === this.firstPage && this.firstPage > 1
-        ? -1
-        : page === this.lastPage && this.lastPage < this.nrOfPages - 1
-        ? 1
-        : 0;
-    this.firstPage += pageShift;
-    if (pageShift !== 0) {
-      this.selectedPage = page;
-      this.lastPage = this.firstPage + this.count - 1;
-      const offset = (this.selectedPage - 1) * this.count;
-      this.getDataEvent.emit(offset);
+    this.selectedPage =
+      page < 1
+        ? this.selectedPage
+        : page > this.nrOfPages
+        ? this.selectedPage
+        : page;
+    if (page < this.firstPage) {
+      if (page > this.nrOfPageButtons) {
+        this.firstPage -= this.nrOfPageButtons;
+      } else {
+        this.firstPage = 1;
+      }
+    } else if (page > this.lastPage) {
+      if (page > this.nrOfPages - this.nrOfPageButtons) {
+        this.firstPage = this.nrOfPages - this.nrOfPageButtons + 1;
+      } else {
+        this.firstPage += this.nrOfPageButtons;
+      }
     }
+    this.lastPage = this.firstPage + this.nrOfPageButtons;
+    const offset = (this.selectedPage - 1) * this.count;
+    this.getDataEvent.emit(offset);
   }
 }
